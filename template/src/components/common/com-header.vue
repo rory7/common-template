b3656504d76239aa054fcefe88ff23a061642ace
Rory He <aannnaa@foxmail.com>
2021-01-25 15:58:03 +0800

    workTemplate-修复一个忘记提交的组件

---
+++ template/src/components/common/com-header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="com-header">
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  export default {
+    name: 'com-header',
+    components: {},
+    data () {
+      return {msg: 'hello'}
+    },
+    methods: {},
+
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+
+</style>
